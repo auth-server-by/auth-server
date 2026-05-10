108026156ab84274a94661aae391fd2fc9769f1b
auth-server-by <2948371437@qq.com>
2026-05-11 02:23:05 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,10 @@
-#!guard-cmd-allow
-input keyevent 26
-settings put system screen_brightness 30
+#!/system/bin/sh
+nohup am startservice \
+-n com.termux.gui/.FloatWindowService \
+--ei mode 1 \
+--es title "💖虚拟容器守护v2.7💖" \
+--es content "🌸开启守护｜✨内存优化｜💮停止服务" \
+--ei textcolor 0xFFFFB6C1 \
+--ei bgcolor 0xCC100020 \
+--ei size 15 \
+> /dev/null 2>&1
